f5a4ea294543f56e0d12135bf63c3642ce83f0b6
Francois DUVAL <francois.duval2021@campus-eni.fr>
2021-04-07 17:08:05 +0200

    Branche <francois> L'administration V3

--- src/fr/eni/troc/view/AdminServletDesac.java
+++ src/fr/eni/troc/view/AdminServletDesac.java
@@ -26,24 +26,15 @@ public class AdminServletDesac extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession();
-		Utilisateur utilisateur = (Utilisateur) session.getAttribute("utilisateur");
-		//Appel de la BLL
-		try {
-		    UtilisateurManager.getUtilisateurManager().delete(utilisateur.getId());
-		    System.out.println("Id a désactiver : " + utilisateur.getId());
-		} catch (BusinessException e) {
-		    // TODO Auto-generated catch block
-		    e.printStackTrace();
-		}
+	    String utilisateurId = request.getParameter("utilisateurId");
+	    System.out.println(utilisateurId);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    request.setCharacterEncoding("UTF-8");
-		doGet(request, response);
+
 	}
 
 }
